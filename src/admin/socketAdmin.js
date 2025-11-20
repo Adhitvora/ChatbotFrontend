a0b0a7428637622f31b771397b250fd84a3fb854
Adhitvora <voraadhit2096@gmail.com>
2025-11-20 16:44:02 +0530

    change url

--- src/admin/socketAdmin.js
+++ src/admin/socketAdmin.js
@@ -1,7 +1,7 @@
 // src/admin/socketAdmin.js
 import { io } from "socket.io-client";
 
-const BACKEND = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+const BACKEND = import.meta.env.VITE_BACKEND_URL;
 
 export const adminSocket = io(BACKEND, {
     transports: ["websocket"],

456aa7bcb35e9d720c85b578e3c01aaa47df6950
Adhitvora <voraadhit2096@gmail.com>
2025-11-20 17:00:28 +0530

    change URL

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const BACKEND = "http://localhost:4000";
+const BACKEND = "https://chatbot-e82s.onrender.com";
 export const socket = io(BACKEND, { autoConnect: true });
